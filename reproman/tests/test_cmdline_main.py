003be169cf7b27d2e4ceb635f6664aa21ad54c16
Austin Macdonald <austin@redhat.com>
2022-12-14 16:38:39 -0500

    this change looks only necessary for fedora, reverting for now

--- reproman/tests/test_cmdline_main.py
+++ reproman/tests/test_cmdline_main.py
@@ -82,8 +82,7 @@ def test_help_np():
                  {'Commands for manipulating computation environments',
                   'Miscellaneous commands',
                   'General information',
-                  })
-                  # 'Global options'})
+                  'Global options'})
 
 # MJT - This test incorrectly tests how the create, ls, install, etc. commands
 # work as they now prompt for missing args rather than display a usage message.
