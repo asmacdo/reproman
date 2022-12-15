a94544802f7137bfa52c37d45fffa6765ac30c30
Austin Macdonald <austin@redhat.com>
2022-12-14 19:44:47 -0500

    pls dont judge these hacks

--- docker-test-script.sh
+++ docker-test-script.sh
@@ -1,5 +1,2 @@
-python3.7 -m pip install -e /home/austin/reproman[tests]
-python3.7 -m pip install chardet
-cd /home/austin/reproman
-
-pytest -rA
+source venvs/reproman/bin/activate
+py.test -rA --integration
